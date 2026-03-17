ebf0b1a45bd38cc6e55b2e3bc6293f62b9ec6144
Aabid Husain <mraabid01@gmail.com>
2026-03-17 10:00:51 +0530

    Updated some previous code

--- Day1/logicalOpe.java
+++ Day1/logicalOpe.java
@@ -1,3 +1,16 @@
 public class logicalOpe {
-    
+    public static void main(String[] args){
+        // & , | , !
+
+        int num1 = 5 , num2 = 4 , num3 = 6;
+
+        if (num1 - num2 == 1 && num3 - num1 == 1){
+            System.out.println("First True");
+        }
+
+        if (num2 * num3 <= num3 * num1 | num2 * num3 >= num3 * num1){
+            System.out.println("Second True");
+        }
+
+    }
 }
