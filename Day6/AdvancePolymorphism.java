6261dd837f62eead59536548a6fa84e55fbadc56
Aabid Husain <mraabid01@gmail.com>
2026-03-17 11:55:04 +0530

    Updated OOPS Concept

--- Day6/AdvancePolymorphism.java
+++ Day6/AdvancePolymorphism.java
@@ -1,29 +1,33 @@
 package Day6;
 
-class Employee{
-    double calculateSalary(){
+class Employee {
+    double calculateSalary() {
         return 0;
     }
 }
 
-class FullTimeEmployee extends Employee{
-    double calculateSalary(){
+class FullTimeEmployee extends Employee {
+    double calculateSalary() {
         return 25000;
     }
 }
 
-class PartTimeSalary extends Employee{
-    double calculateSalary(){
+class PartTimeSalary extends Employee {
+    double calculateSalary() {
         return 20000;
     }
 }
 
 public class AdvancePolymorphism {
-    public static void main(String[] args){
-        Employee emp;
+    public static void main(String[] args) {
+        Employee emp, fullTimeEmp, partTimeEmp;
 
-        emp = new FullTimeEmployee();
+        emp = new Employee();
+        fullTimeEmp = new FullTimeEmployee();
+        partTimeEmp = new PartTimeSalary();
 
-        System.out.println(emp.calculateSalary());
+        System.out.println("Employee " + emp.calculateSalary());
+        System.out.println("Full Time Employee " + fullTimeEmp.calculateSalary());
+        System.out.println("Part Time Employee" + partTimeEmp.calculateSalary());
     }
 }
