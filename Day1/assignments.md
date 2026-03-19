c94fd5e83929533406765b704012ccd9f4b7fc32
Aabid Husain <mraabid01@gmail.com>
2026-03-19 15:19:08 +0530

    Updated Day1

---
+++ Day1/assignments.md
@@ -0,0 +1,12 @@
+* ## 1st:
+    * Write code to print diffirent type of numbers.
+    * Write code to print area of circle.
+    * Write code to print your 10th score.
+    * Write code to print basic details by taking input using Scanner.
+
+* ## 2nd:
+    * Update progress on Github and tag me to see your Progress and get Certificate of completion on every step by **```MAT Nite Private Limited```**.
+
+* ## 3rd:
+    * Follow **@maiaabidhoon** on social media.
+    * Collab me.
---
+++ Day1/variablesAndDatatype.java
@@ -0,0 +1,24 @@
+public class variablesAndDatatype {
+    public static void main(String[] args){
+        boolean isStudent = true;
+        byte favNumber = 7;
+        short rank = 1000;
+        int age = 22;
+        long mobileNumber = 8795521475L;
+        float pi = 3.14f;
+        double salary = 20000.50;
+        char nameInitial = 'A';
+
+        String name = "Aabid Husain";
+
+        System.out.println("Is Student : " + isStudent);
+        System.out.println("Fav Number : " + favNumber);
+        System.out.println("Rank : " + rank);
+        System.out.println("My Age : " + age);
+        System.out.println("Mobile Number : " + mobileNumber);
+        System.out.println("Pi Value : " + pi);
+        System.out.println("Salary : " + salary);
+        System.out.println("Name Initial : " + nameInitial);
+        System.out.println("Name : " + name);
+    }
+}
