e870d7ed8d419c6670246c0111469ba500fbddfa
Aabid Husain <mraabid01@gmail.com>
2026-03-28 10:05:13 +0530

    Updated Day2 Theory

---
+++ Day02/scannerClassDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class scannerClassDemo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Your Name");
+
+        String name = sc.nextLine();
+
+        System.out.println("Name : " + name);
+        sc.close();
+
+    }
+}
